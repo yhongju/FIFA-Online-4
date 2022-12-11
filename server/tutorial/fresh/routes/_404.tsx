bac09a1a800cb55099133c60fc5275f14cd066b7
Yi Hong-ju <to_hongju@pm.me>
2022-12-11 16:28:15 +0900

    Del fresh, deno tutorials
    Moved to my GitHub/Tutorials

--- server/tutorial/fresh/routes/_404.tsx
+++
@@ -1,9 +0,0 @@
-// Custom 404 page
-
-import { UnknownPageProps } from "$fresh/server.ts";
-
-const NotFoundPage = ({ url }: UnknownPageProps) => {
-  return <p>404 not found: {url.pathname}</p>;
-};
-
-export default NotFoundPage;
