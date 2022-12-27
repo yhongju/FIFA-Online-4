f862d15a161856e3509176d4090fe0bba6174011
Yi Hong-ju <to_hongju@pm.me>
2022-12-27 21:51:02 +0900

    Add index video and Update files
    Modify deno.lock to hash error

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,48 @@
-import { Head } from "$fresh/runtime.ts";
+import { useState } from "preact/hooks";
+import Subscribe from "../components/Subscribe.tsx";
 
-export default function Home() {
+const Index = () => {
+  const [subscribed, setSubscribed] = useState(false);
+
   return (
-    <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
+    // <div class="bg-gradient-to-tr from-red-500 to-purple-400 relative h-screen w-screen">
+    <div class="bg-black">
+      {
+        /* <img
+        class="absolute inset-0 w-full h-full object-cover mix-blend-multiply filter brightness-50"
+        alt="main background image"
+        src="https://source.unsplash.com/random"
+        draggable="false" // Ignore error: Type inference error
+      /> */
+      }
+      <video autoPlay loop={true} class="w-screen h-screen blur-xl">
+        <source src={"index.webm"} class="blur-xl" />
+      </video>
+
+      <div class="absolute inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto text-center">
+        {subscribed
+          ? (
+            <h1 class="font-sans font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-snug">
+              You have subscribed!
+            </h1>
+          )
+          : (
+            <div class="space-y-8">
+              <h1 class="font-sans font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-tight">
+                피파 온라인 4
+                <span class="text-palette-primary inline-block">
+                  선수 추천 서비스
+                </span>
+              </h1>
+              <p class="text-palette-light text-base md:text-lg lg:text-xl">
+                ...
+              </p>
+              {/* <Subscribe setSubscribed={setSubscribed} /> */}
+            </div>
+          )}
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default Index;
--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,16 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        palette: {
+          light: "#F5F3FF",
+          primary: "#7C3AED",
+          dark: "#6D28D9",
+        },
+      },
+    },
+  },
 } as Options;
