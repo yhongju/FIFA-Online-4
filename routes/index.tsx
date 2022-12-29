40f9cae169bd9b8200eb6bcce16ae117ea6cbfba
Yi Hong-ju <to_hongju@pm.me>
2022-12-30 07:34:26 +0900

    Update `index.tsx` with errors
    TODO:
    Fix context issue
    Place new components

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,27 @@
 import { createContext } from "preact";
 import { StateUpdater, useState } from "preact/hooks";
-import StartButton from "../islands/StartButton.tsx";
 
-const StartContext = createContext();
+import StartButton from "../islands/StartButton.tsx";
+import Position from "../components/Position.tsx";
+// TODO:
+// import Payward from "../component/Payward.tsx";
+// import TeamColor from "../component/TeamColor.tsx";
+// import BP from "../component/BP.tsx";
+
+const StartContext = createContext({
+  started: false,
+  setStarted: StateUpdater(false),
+});
 
 const Index = () => {
-  const [started, setStarted] = useState(false);
+  const [started, setStarted] = useState(true);
 
   return (
     <div class="bg-black w-screen h-screen">
       <video
         autoPlay={true}
         loop={true}
-        class="w-full h-full bg-black"
+        class="w-full h-full bg-black inline-block"
         muted={true}
         type="video/webm"
         style={{ filter: "blur(5px)" }}
@@ -20,9 +29,18 @@ const Index = () => {
         <source src={"index.webm"} />
       </video>
       {started
-        ? <div>hello</div>
+        ? (
+          // <div class="bg-rose-800 w-24 h-24 flex">
+          //   <Position />
+          //   {
+          //     /* <Payward />
+          //   <TeamColor />
+          //   <BP /> */
+          //   }
+          // </div>
+        )
         : (
-          <StartContext.Provider value={[started, setStarted]}>
+          <StartContext.Provider value={{ started, setStarted }}>
             <StartButton />
           </StartContext.Provider>
         )}
